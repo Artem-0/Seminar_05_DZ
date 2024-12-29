3f391f12a3e61e47b3d889f3d0cd8f04799ca0ca
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-29 23:20:25 +0300

    Выполнил задачу 34

---
+++ ex34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Здалача 34. Задайте массив, заполненный случайными полодительными
+// трёхзначными числами. Напишите программу, которая покажет количество
+// чётных чисел в массиве.
+
+int[] GenerateArray(int sizeArray, int leftRange, int rightRange)
+{
+    int[] newArray = new int[sizeArray];
+    Random rand = new Random();
+
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return newArray;
+}
+
+void PrintArray2(int[] arrayForPrint)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] %2 == 0)
+            count++;
+    }
+    return count;
+}
+
+// -------------------------------------------------------------------------
+
+int[] array = GenerateArray(4, 100, 999);
+PrintArray2(array);
+System.Console.WriteLine("Количество чётных чисел в массиве: " + CountEvenNumbers(array));
